3622534947581070389486eb2db2d799a8d3ce3e
Stanton Huang <stantonhuang11@gmail.com>
2019-12-12 09:59:00 -0800

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {useState, useReducer, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [operation, setOperation] = useState('');
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    switch (operation) {
+      case '+':
+        setResult((input1 + input2).toFixed(input1.length));
+        break;
+      case '-':
+        setResult((input1 - input2).toFixed(input1.length));
+        break;
+      case '*':
+        setResult((input1 * input2).toFixed(input1.length));
+        break;
+      case '/':
+        setResult((input1 / input2).toFixed(input1.length));
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{result}</div>
+        <input type="text" onChange={(e) => setInput1(parseFloat(e.target.value))}></input>
+        <input type="text" onChange={(e) => setInput2(parseFloat(e.target.value))}></input>
+        <button onClick={() => setOperation('+')}>+</button>
+        <button onClick={() => setOperation('-')}>-</button>
+        <button onClick={() => setOperation('*')}>*</button>
+        <button onClick={() => setOperation('/')}>/</button>
+        <button onClick={(e) => handleSubmit(e)}>=</button>
+        
       </header>
     </div>
   );
